91bf7a7a97662ac349d7315f8d3916d7fa47ed38
kunal <kunal.aage@weenggs.in>
2024-12-16 16:03:34 +0530

    fix package

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container  } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 // import Particle from "../Components/Particles/Particle";
 import Techstack from "../Components/TechStack/TechStack";
 import ToolStack from "../Components/ToolStack/ToolStack";
